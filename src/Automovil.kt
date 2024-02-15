9a1245a22d539046c2309f4656c2a8ca98863097
rmungar <mgraul@iesrafaelalberti.es>
2024-02-15 21:27:07 +0100

    continuar

--- src/Automovil.kt
+++ src/Automovil.kt
@@ -1,2 +1,37 @@
-class Automovil {
+class Automovil(marca:String, modelo:String, capacidadCombustible:Float, combustibleActual: Float, kilometrosActuales: Int, val esElectrico: Boolean, var condicionBritania: Boolean):Vehiculo(marca, modelo, capacidadCombustible, combustibleActual, kilometrosActuales) {
+
+    init {
+        require(esElectrico || !esElectrico) {"El campo esElectrico ha de ser true o false"}
+        require(condicionBritania || !condicionBritania) {"El campo condicionBritania ha de ser true o false"}
+    }
+
+    override fun calcularAutonomia(): Int {
+        if (esElectrico) {
+            return (combustibleActual * 5).toInt()
+        }else{
+            return super.calcularAutonomia()
+        }
+    }
+
+    fun cambiarCondicionBritania(nuevaCondicion: Boolean){
+        if (nuevaCondicion){
+            condicionBritania = false
+            println("Ahora $marca $modelo tiene el volante a la izquierda")
+        }
+        else{
+            condicionBritania = true
+            println("Ahora $marca $modelo tiene el volante a la derecha")
+        }
+    }
+
+    fun realizarDerrape(): Float{
+        if (calcularAutonomia() - 5.00 > 0.00){
+            combustibleActual - 5.00
+            return combustibleActual
+        }
+        else{
+            println("No hay combustible suficiente para derrapar")
+            return combustibleActual
+        }
+    }
 }
--- src/Vehiculo.kt
+++ src/Vehiculo.kt
@@ -1,2 +1,45 @@
-class Vehiculo {
+import kotlin.math.roundToInt
+
+abstract class Vehiculo (val marca: String, val modelo: String, val capacidadCombustible: Float, var combustibleActual: Float, var kilometrosActuales: Int){
+    init {
+        require(marca.isNotBlank()) {"El campo de marca no puede estar vacío"}
+        marca.replaceFirstChar { it.uppercase() }
+        require(modelo.isNotBlank()) {"Ell campo de modelo no puede estar vacío"}
+        modelo.replaceFirstChar { it.uppercase() }
+        require(capacidadCombustible > 0.00) {"La capacidad de combustible no puede ser menor a 0.0L"}
+        require(combustibleActual >= 0.0) {"El combustible actual no puede ser menor a 0.0L"}
+        require(kilometrosActuales >= 0) {"Los kilométros actuales no pueden ser menor a 0Km"}
+    }
+    open fun calcularAutonomia(): Int{
+        return (combustibleActual*10).roundToInt()
+    }
+    fun realizaViaje(distancia: Int):Int {
+        var distanciaRestante: Int = 0
+        if (calcularAutonomia() > distancia){
+            if ((calcularAutonomia() - distancia) > 0){
+                return distanciaRestante
+            }
+            else if ((calcularAutonomia() - distancia) < 0){
+                distanciaRestante =(distancia - calcularAutonomia()).redondear()
+                return distanciaRestante
+            }
+        }
+        else{
+            distanciaRestante =(distancia - calcularAutonomia()).redondear()
+            return distanciaRestante
+        }
+    }
+    fun repostar(cantidad:Float): Float{
+        if (combustibleActual + cantidad > capacidadCombustible){
+            combustibleActual = capacidadCombustible
+            return combustibleActual
+        }
+        else{
+            combustibleActual += cantidad
+            return combustibleActual
+        }
+    }
+    open fun obtenerInformacion(): String{
+        return "$marca $modelo puede recorrer ${calcularAutonomia()}Kms actualmente, ya cuenta con ${kilometrosActuales}Kms recorridos"
+    }
 }
